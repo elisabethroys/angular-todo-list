456cd2dea8479ab59502c82301123d709799c51d
elisabethroys <125894666+elisabethroys@users.noreply.github.com>
2025-09-10 08:23:34 +0200

    Added extensions functionality

--- src/app/todo/todo-list/todo-list.component.ts
+++ src/app/todo/todo-list/todo-list.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { TodoService } from '../services/todo.service';
 import { Todo } from '../models/todo';
@@ -11,22 +12,34 @@ import { Todo } from '../models/todo';
 })
 export class TodoListComponent {
   public todos$ = new Observable<Todo[]>();
+  public filteredTodos$ = new Observable<Todo[]>();
+  showOnlyCompleted = false;
 
   constructor(private readonly todoService: TodoService) {}
 
   ngOnInit(): void {
     this.todos$ = this.todoService.getTodos();
-    this.todos$.subscribe((todos) => {
-      todos.forEach((todo) => {
-        console.log(todo.title);
-      });
-    });
+    this.applyFilter();
+  }
+
+  applyFilter(): void {
+    this.filteredTodos$ = this.todos$.pipe(
+      map((todos) =>
+        this.showOnlyCompleted ? todos.filter((todo) => todo.completed) : todos.filter((todo) => !todo.completed)
+      )
+    );
+  }
+
+  filterList() {
+    this.showOnlyCompleted = !this.showOnlyCompleted;
+    this.applyFilter();
   }
 
   updateTodo(todo: Todo): void {
     this.todoService.updateTodo(todo).subscribe({
       next: (updated) => {
         console.log('Todo oppdatert:', updated);
+        this.applyFilter();
       },
       error: (err) => {
         console.error('Feil ved oppdatering:', err);
@@ -37,7 +50,7 @@ export class TodoListComponent {
   newTodo(title: string): void {
     this.todoService.addTodo(title).subscribe({
       next: () => {
-        this.todos$ = this.todoService.getTodos();
+        this.applyFilter();
       },
       error: (err) => {
         console.error('Feil ved POST:', err);
